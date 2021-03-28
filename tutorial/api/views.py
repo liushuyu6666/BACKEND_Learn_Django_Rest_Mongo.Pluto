6538d9df9810c284cdf9259a12d2d519ef34228b
liushuyu6666 <liushuyu6666@users.noreply.github.com>
2021-03-28 12:30:32 -0400

    handle with URL

--- tutorial/api/views.py
+++ tutorial/api/views.py
@@ -6,4 +6,6 @@ from rest_framework.permissions import AllowAny
 @api_view()
 @permission_classes([AllowAny])
 def firstFunction(request):
+    print(request.query_params)
+    print(request.query_params['id'])
     return Response({'message': "hello there!"})
